a32a53a3f768407970392f31d53828d271059d29
beast0091 <darkpixel25555@outlook.com>
2024-07-06 21:00:44 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,32 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React from 'react';
+import './index.css'; // You can create this CSS file to style your page
 
-export default function Home() {
+const IndexPage = () => {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by Earning <code>pages/index.js</code>
-        </p>
+      <header className="header">
+        <h1>Welcome to my page!</h1>
+        <p>Explore my links below:</p>
+      </header>
+      <main className="main">
+        <div className="links-container">
+          <a href="https://example.com" className="link-item">
+            Example Website
+          </a>
+          <a href="https://github.com" className="link-item">
+            GitHub Profile
+          </a>
+          <a href="https://twitter.com" className="link-item">
+            Twitter Profile
+          </a>
+          {/* Add more links as needed */}
+        </div>
       </main>
-
-      <Footer />
+      <footer className="footer">
+        <p>© 2024 Your Name</p>
+      </footer>
     </div>
-  )
-}
+  );
+};
+
+export default IndexPage;
